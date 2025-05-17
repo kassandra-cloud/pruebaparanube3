dbd7edb04e3afe6a867d5f0849ae569798e7399b
Kassandra Cloud <kassramosveg@gmail.com>
2025-05-16 21:21:22 -0400

    Primer commit: Estructura inicial del proyecto

---
+++ Tesis_JuntaVecino/Tesis_JuntaVecino/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Tesis_JuntaVecino project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tesis_JuntaVecino.settings')
+
+application = get_asgi_application()
